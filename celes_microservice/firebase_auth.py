b95318135c240f54fd06514add004c1f5810ef5d
Samir <98545750+Samir221@users.noreply.github.com>
2023-12-10 22:17:40 -0500

    Update firebase_auth.py

--- celes_microservice/firebase_auth.py
+++ celes_microservice/firebase_auth.py
@@ -13,6 +13,17 @@ from dotenv import load_dotenv
 import os
 
 
+firebaseConfig = {
+  "apiKey": os.getenv("FIREBASE_API_KEY"),
+  "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+  "projectId": os.getenv("FIREBASE_PROJECT_ID"),
+  "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
+  "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
+  "appId": os.getenv("FIREBASE_APP_ID"),
+  # "databaseURL": os.getenv("FIREBASE_DATABASE_URL") # Uncomment if you have this
+}
+
+
 load_dotenv()
 
 
